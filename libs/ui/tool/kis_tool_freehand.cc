19f0162015f1ef9e3d986512034a07c0798bf968
alie <agkuzt@gmail.com>
2026-08-02 15:56:39 +0300

    Increase freehand resize speed

--- libs/ui/tool/kis_tool_freehand.cc
+++ libs/ui/tool/kis_tool_freehand.cc
@@ -372,10 +372,12 @@ void KisToolFreehand::continueAlternateAction(KoPointerEvent *event, AlternateAc
     const qreal scaleCoeff = effectiveMaxBrushSize / effectiveMaxDragSize;
     const qreal sizeDiff = scaleCoeff * offset.x() ;
 
+    const qreal sizeMult = 2.0; // resize speed multiplier
+
     if (qAbs(sizeDiff) > 0.01) {
         KisPaintOpSettingsSP settings = currentPaintOpPreset()->settings();
 
-        qreal newSize = m_lastPaintOpSize + sizeDiff;
+        qreal newSize = m_lastPaintOpSize + sizeDiff * sizeMult;
 
         if (action == ChangeSizeSnap) {
             newSize = qMax(qRound(newSize), 1);
@@ -495,5 +497,3 @@ KisOptimizedBrushOutline KisToolFreehand::getOutlinePath(const QPointF &document
     else
         return KisOptimizedBrushOutline();
 }
-
-
