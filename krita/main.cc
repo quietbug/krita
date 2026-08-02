6b76d1b6ec4edc9ebcb94f544ab7b24d139e5315
alie <agkuzt@gmail.com>
2026-08-02 15:56:39 +0300

    Disable X11 menu accelerators

--- krita/main.cc
+++ krita/main.cc
@@ -94,6 +94,9 @@
 #if defined HAVE_KCRASH
 #include <kcrash.h>
 #elif defined USE_DRMINGW
+
+extern void qt_set_sequence_auto_mnemonic(bool);
+
 namespace
 {
 template<typename T, typename U>
@@ -233,6 +236,8 @@ extern "C" MAIN_EXPORT int MAIN_FN(int argc, char **argv)
     if (!qEnvironmentVariableIsSet("QT_QPA_PLATFORM")) {
         qputenv("QT_QPA_PLATFORM", "xcb");
     }
+    // Disable window menu hotkeys (accelerators)
+    qt_set_sequence_auto_mnemonic(false);
 #elif defined Q_OS_WIN
     if (!qEnvironmentVariableIsSet("QT_QPA_PLATFORM")) {
         qputenv("QT_QPA_PLATFORM", "windows:darkmode=1");
